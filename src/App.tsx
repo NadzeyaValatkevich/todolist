73883e32705e3da90999974bb7e6b6473a092851
NadzeyaValatkevich <nadkonadzya@rambler.ru>
2022-11-09 16:15:21 +0300

    Components&Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Todolist} from "./Todolist";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+
+    const tasks1 = [
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'React', isDone: false}
+    ]
+
+    const tasks2 = [
+        {id: 1, title: 'romantic', isDone: true},
+        {id: 2, title: 'comady', isDone: false},
+        {id: 3, title: 'triller', isDone: true}
+    ]
+
+
+    return (
+        <div className="App">
+            <Todolist title={'What to learn'} tasks={tasks1}/>
+            <Todolist title={'What to watch'} tasks={tasks2}/>
+        </div>
+    )
 }
 
-export default App;
